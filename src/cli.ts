d35242266eb4073edd4931a524e46acabf32bf84
AOKI Yuto <aoki@u-ne.co>
2024-06-09 07:05:35 +0900

    add cli

---
+++ src/cli.ts
@@ -0,0 +1,48 @@
+// scrambler DEFINITION INPUT
+
+import { createScrambler } from '.'
+
+// DEFINITION FORMAT:
+// {digits}-{n1}-{n2}-{seed}-{stages}
+
+const definitionPattern = /^(\d+)-(\d+)-(\d+)-(\d+)-(\d+)$/
+
+const [,, definition, input] = process.argv
+
+if (!definitionPattern.test(definition)) {
+  error('Invalid definition Pattern. Use: digits-n1-n2-seed-stages')
+}
+
+const [digits, n1, n2, seed, stages] = definition.split('-').map(Number)
+
+if (Number.isNaN(digits) || Number.isNaN(n1) || Number.isNaN(n2) || Number.isNaN(seed) || Number.isNaN(stages)) {
+  error('Invalid definition values. Use: digits-n1-n2-seed-stages')
+}
+
+if (input === undefined || !/^\d+$/.test(input)) {
+  error('Input must be a positive number')
+}
+
+const scrambler = createScrambler({
+  digits,
+  n1: BigInt(n1),
+  n2: BigInt(n2),
+  seed,
+  stages
+})
+
+const from = BigInt(input)
+
+if (from < 0) {
+  error('Input must be a positive number')
+}
+
+const to = scrambler.scramble(from)
+
+process.stdout.write(`${to}\n`)
+
+function error (message: string): never {
+  console.error(message)
+  // eslint-disable-next-line no-process-exit
+  process.exit(1)
+}
