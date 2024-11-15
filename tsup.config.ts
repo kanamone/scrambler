6d6917dfbc3b5ee49035fba1c7854da7ae71a934
naporin0624 <naporin0624@gmail.com>
2024-11-15 08:06:08 +0000

    chore: add entry scripts index and cli

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: {
+    index: "src/index.ts",
+    cli: "src/cli.ts",
+  },
   sourcemap: true,
   clean: true,
   dts: true,
